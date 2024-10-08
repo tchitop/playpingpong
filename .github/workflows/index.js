eb6b17a451a3c8c4c7031a7e011a4f139d64252f
tchitop <168271881+tchitop@users.noreply.github.com>
2024-10-08 18:34:33 +0200

    index.js

---
+++ .github/workflows/index.js
@@ -0,0 +1,22 @@
+require('dotenv').config(); // lädt Umgebungsvariablen aus .env
+
+const { Client, GatewayIntentBits } = require('discord.js');
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds, 
+        GatewayIntentBits.GuildMessages, 
+        GatewayIntentBits.MessageContent
+    ]
+});
+
+client.once('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('messageCreate', message => {
+    if (message.content === '!ping') {
+        message.channel.send('Pong!');
+    }
+});
+
+client.login(process.env.DISCORD_TOKEN); // Sicherer Zugriff auf Token
